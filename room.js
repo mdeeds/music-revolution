b9b4d7e2a9ff16485d89a2fb7807ae16f0196e1a
mdeeds <mdeeds@penguin>
2024-06-12 17:50:15 -0700

    Nicer room styling.

--- room.js
+++ room.js
@@ -1,30 +1,97 @@
 class Room {
+    // .---------------------------.
+    // | room                      |
+    // | .-----------------------. |
+    // | | room-name             | |
+    // | .-----------------------. |
+    // | .-----------------------. |
+    // | | seat-area             | |
+    // | | .-------------------. | |
+    // | | | recording-area    | | |
+    // | | | .---..----..----. | | |
+    // | | | |rec||play||seat| | | |
+    // | | | .---..----..----. | | |
+    // | | .-------------------. | |
+    // | | .-------------------. | |
+    // | | | visitor-area      | | |
+    // | | | .----..----.      | | |
+    // | | | |seat||seat|      | | |
+    // | | | .----..----.      | | |
+    // | | | .----..----.      | | |
+    // | | | |seat||seat|      | | |
+    // | | | .----..----.      | | |
+    // | | .-------------------. | |
+    // | .-----------------------. |
+    // .---------------------------.
     constructor(roomName, container, localPawn, recorder) {
         container.classList.add('rooms');
         this.roomName = roomName;
         this.participants = new Set();
         this.audioDataBuffers = {};
-        this.roomDiv = document.createElement('div');
-        this.roomDiv.classList.add('room');
-        const roomNameDiv = document.createElement('div');
-        roomNameDiv.classList.add('room-name');
-        roomNameDiv.innerText = roomName;
-        this.roomDiv.appendChild(roomNameDiv);
-        
-        this.seatsDiv = document.createElement('div');
-        this.seatsDiv.classList.add('seats');
-        this.seatsDiv.innerText = 'seats';
-        this.roomDiv.appendChild(this.seatsDiv);
-
-        this.micDiv = document.createElement('div');
-        this.micDiv.innerText = 'mic';
-        this.roomDiv.appendChild(this.micDiv);
-
         this.localPawn = localPawn;
         this.recorder = recorder;
-        this.seatsDiv.addEventListener('click', this.addToRoom.bind(this, localPawn));
-        this.micDiv.addEventListener('click', this.addToMic.bind(this, localPawn));
+        this.roomDiv = this._makeRoom();
         container.appendChild(this.roomDiv);
+
+        this.seatsDiv.addEventListener('click', this.addToRoom.bind(this, localPawn));
+        this.micSeatDiv.addEventListener('click', this.addToMic.bind(this, localPawn));
+    }
+
+    _makeRoom() {
+        const roomDiv = document.createElement('div');
+        roomDiv.classList.add('room');
+        roomDiv.appendChild(this._makeRoomName());
+        roomDiv.appendChild(this._makeSeatArea());
+        return roomDiv;
+    }
+
+    _makeRoomName() {
+        const roomNameDiv = document.createElement('div');
+        roomNameDiv.classList.add('room-name');
+        roomNameDiv.innerText = this.roomName;
+        return roomNameDiv
+    }
+
+    _makeSeatArea() {
+        const seatAreaDiv = document.createElement('div');
+        seatAreaDiv.classList.add('seat-area');
+
+        seatAreaDiv.appendChild(this._makeRecordingArea());
+        seatAreaDiv.appendChild(this._makeVisitorArea());
+        return seatAreaDiv;
+    }
+
+    _makeVisitorArea() {
+        const visitorArea = document.createElement('div');
+        visitorArea.classList.add('visitor-area');
+        for (let i = 0; i < 4; ++i) {
+            const seat = this._makeSeat();
+            visitorArea.appendChild(seat);
+            this.seatsDiv = seat;
+        }
+        return visitorArea;
+    }
+
+    _makeRecordingArea() {
+        const recordingAreaDiv = document.createElement('div');
+        recordingAreaDiv.classList.add('recording-area');
+        const recButton = document.createElement('button');
+        recButton.innerText = 'rec';
+        recButton.classList.add('rec');
+        recordingAreaDiv.appendChild(recButton);
+        const playButton = document.createElement('button');
+        playButton.innerText = 'play';
+        playButton.classList.add('play');
+        recordingAreaDiv.appendChild(playButton);
+        this.micSeatDiv = this._makeSeat();
+        recordingAreaDiv.appendChild(this.micSeatDiv);
+        return recordingAreaDiv;
+    }
+
+    _makeSeat() {
+        const seatDiv = document.createElement('div');
+        seatDiv.classList.add('seat');
+        return seatDiv;
     }
 
     addToRoom(pawn) {
@@ -33,7 +100,7 @@ class Room {
         
     }
     addToMic(pawn) {
-        this.micDiv.appendChild(pawn.element);
+        this.micSeatDiv.appendChild(pawn.element);
         this.recorder.enableRecording();
     }
 }
