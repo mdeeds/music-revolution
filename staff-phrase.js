3faeb27073be5460ce93971196ba2df20b1c84e9
mdeeds <mdeeds@penguin>
2024-06-12 17:56:19 -0700

    Staff can be edited now

--- staff-phrase.js
+++ staff-phrase.js
@@ -3,6 +3,8 @@ class StaffPhrase {
         const div = document.createElement('div');
         div.innerText= "=&4=";
         div.classList.add('staff');
+        div.setAttribute('contenteditable', 'plaintext-only');
+        div.setAttribute('spellcheck', 'false');
         container.appendChild(div);
     }
 }
